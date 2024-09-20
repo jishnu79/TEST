0924268366fe4792d9a859231d05c833a957499d
jishnu79 <jishnu@toolfe.com>
2024-09-20 19:01:06 +0530

    backent setup

---
+++ backent/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+dotenv.config();
+
+const port = process.env.PORT; 
+
+mongoose
+    .connect(process.env.MONGO_DB)
+    .then(() => {
+        console.log(`DataBase connected`)
+    }).catch((e) => {
+        console.log(e);
+    })
+
+app.listen(port, () => {
+    console.log(`User Server is running at ${port}`);  
+  });
--- frontent/pages/index.tsx
+++ frontent/pages/index.tsx
@@ -1,9 +1,5 @@
-
-
-
-
 export default function Home() {
   return (
-  <h1>kannan</h1>
+  <h1 className="bg-red-500">kannan</h1>
   );
 }
